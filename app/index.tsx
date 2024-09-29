da5c0ff909fc65becee0a38bba4f4e30b8752221
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-09-28 23:06:44 -0500

    Added tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-// import { useAuth } from "@clerk/clerk-expo";
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
 const Page = () => {
-//   const { isSignedIn } = useAuth();
+  const { isSignedIn } = useAuth();
 
-//   if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
 
   return <Redirect href="/(auth)/welcome"/>;
 };
