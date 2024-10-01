3247c0f348aab57ee95e43ece1c8e97adf615d52
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-09-30 20:35:26 -0500

    #14 Added Log out button

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native';
+import { icons } from '@/constants';
+import { useAuth } from '@clerk/clerk-expo';
+import { router } from 'expo-router';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
+    const { signOut } = useAuth();
+    
+    const handleSignOut = () => {
+        signOut();
+        router.replace("/(auth)/sign-in");
+      };
+      
     return (
         <SafeAreaView>
             <Text>Profile</Text>
+
+            {/* Sign Out button */}
+            <TouchableOpacity
+                onPress={handleSignOut}
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+              >
+                <Image source={icons.out} className="w-4 h-4" />
+              </TouchableOpacity>
         </SafeAreaView>
+
     )
 }
 
