e59167a06300a2edb79182b26869c80b268b98c1
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-10-14 22:09:16 -0500

    date format

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,14 @@
-
+export function formatTime(minutes: number): string {
+    const formattedMinutes = +minutes?.toFixed(0) || 0;
+  
+    if (formattedMinutes < 60) {
+      return `${minutes} min`;
+    } else {
+      const hours = Math.floor(formattedMinutes / 60);
+      const remainingMinutes = formattedMinutes % 60;
+      return `${hours}h ${remainingMinutes}m`;
+    }
+  }
 
 export function formatDate(dateString: string): string {
     const date = new Date(dateString);
